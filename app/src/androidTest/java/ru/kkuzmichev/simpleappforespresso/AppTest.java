072d3513521b155e4b54aa6e618bf47e4a8fea38
Purpurova-k <katrin.purpurova@gmail.com>
2022-08-23 13:51:14 +0300

    Test for Custom ViewMatcher and Custom ViewAssertion added

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/AppTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/AppTest.java
@@ -1,26 +1,36 @@
 package ru.kkuzmichev.simpleappforespresso;
 
+import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+
 import static org.hamcrest.core.AllOf.allOf;
 
 import android.content.Intent;
 
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.espresso.IdlingRegistry;
+import androidx.test.espresso.ViewAction;
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -84,7 +94,17 @@ public class AppTest {
         gallery.check(matches(isDisplayed()));
         gallery.perform(click());
 
-        ViewInteraction galleryItem = onView(allOf(withId(R.id.item_number), withText("6")));
-        galleryItem.check(matches(isDisplayed()));
+
+        ViewInteraction recycleView = onView(withId(R.id.recycle_view));
+        recycleView.check(
+                matches(CustomViewMatcher.recyclerViewSizeMatcher(10))
+        );
+
+        recycleView.check(CustomViewAssertions.isRecyclerView());
+
+
+        recycleView
+                .perform(RecyclerViewActions.scrollTo(hasDescendant(withText("7"))))
+                .check(matches(isDisplayed()));
     }
 }
